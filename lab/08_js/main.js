df18be11fde38ba367e0881f2cab18601b291fb8
Maria Zabolotnaya <Maria Zabolotnaya>
2017-03-17 10:49:11 -0400

    blah

---
+++ lab/08_js/main.js
@@ -0,0 +1,25 @@
+function checkerboard(width, height) {
+    var checkerboard = "";
+
+    for (j = 0; j < 4; j++){
+        if (j % 2) {
+            for (i = 0; i < 8; i++) {
+                if ( i % 2 ){
+                    checkerboard = checkerboard + "#";
+                } else {
+                    checkerboard = checkerboard + " ";
+                }
+            }
+        } else {
+            for (i = 0; i < 8; i++) {
+                if ( i % 2 ){
+                    checkerboard = checkerboard + " ";
+                } else {
+                    checkerboard = checkerboard + "#";
+                }
+            }
+        }
+        checkerboard = checkerboard + "\n";
+        console.log(checkerboard);
+    }
+}
