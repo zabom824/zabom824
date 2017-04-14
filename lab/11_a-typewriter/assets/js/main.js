7d1722b530643180d6a78c90d1fc85ff3f41c605
Maria Zabolotnaya <zabom824@newschool.edu>
2017-04-14 13:18:59 -0400

    xkjvb

---
+++ lab/11_a-typewriter/assets/js/main.js
@@ -0,0 +1,62 @@
+var container = document.getElementById('container');
+var letters = {
+    KeyA: {},
+    KeyB: {},
+    KeyC: {},
+    KeyD: {},
+    KeyE: {},
+    KeyF: {},
+    KeyG: {},
+    KeyH: {},
+    KeyI: {}, 
+    KeyJ: {},
+    KeyK: {},
+    KeyL: {},
+    KeyM: {},
+    KeyN: {},
+    KeyO: {},
+    KeyP: {},
+    KeyQ: {},
+    KeyR: {},
+    KeyS: {},
+    KeyT: {},
+    KeyU: {},
+    KeyV: {},
+    KeyW: {},
+    KeyX: {},
+    KeyY: {},
+    KeyZ: {}
+};
+
+document.addEventListener('keydown', function(event){
+    if (event.keyCode >= 65 && event.keyCode <= 90) {
+        var div = document.createElement('div');
+        div.classList.add('letter');
+        // add capital class
+        if (event.shiftKey == true) {
+            div.classList.add('capital');
+        };
+        div.style.backgroundColor = letters[event.code].bgColor;
+        container.appendChild(div);
+    } else if (event.keyCode == 32) {
+        var div = document.createElement('div');
+        div.classList.add('space');
+        container.appendChild(div);
+    } else if (event.keyCode == 8) {
+        container.removeChild(container.lastChild);
+    }
+});
+
+//________________________________________________
+        // eg: a, b, z
+        // console.log("key: " + event.key);
+        // eg: KeyA, KeyB, etc
+        // console.log("code: " + event.code);
+        // eg: 65, 68, etc
+        // console.log("code: " + event.keyCode);
+        // log the color
+        // console.log("color: " + letters[event.code].bgColor);
+        // change the background color
+        // document.documentElement.style.backgroundColor = letters[event.code].bgColor;
+        // add a letter to container
+        // container.innerHTML += event.key;
