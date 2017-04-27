7bde23c978c7f6d4a1a83ebb5ecb9bcf5abb0205
Maria Zabolotnaya <zabom824@newschool.edu>
2017-04-26 21:57:47 -0400

    typewriter WIP

--- lab/11_a-typewriter/assets/js/main.js
+++ lab/11_a-typewriter/assets/js/main.js
@@ -1,43 +1,99 @@
 var container = document.getElementById('container');
+
+
 var letters = {
-    KeyA: {},
-    KeyB: {},
-    KeyC: {},
-    KeyD: {},
-    KeyE: {},
-    KeyF: {},
-    KeyG: {},
-    KeyH: {},
-    KeyI: {}, 
-    KeyJ: {},
-    KeyK: {},
-    KeyL: {},
-    KeyM: {},
-    KeyN: {},
-    KeyO: {},
-    KeyP: {},
-    KeyQ: {},
-    KeyR: {},
-    KeyS: {},
-    KeyT: {},
-    KeyU: {},
-    KeyV: {},
-    KeyW: {},
-    KeyX: {},
-    KeyY: {},
-    KeyZ: {}
+    KeyA: {
+        url:"assets/media/svg/Asset1.svg"
+    },
+    KeyB: {
+        url:"assets/media/svg/Asset24.svg"
+    },
+    KeyC: {
+        url:"assets/media/svg/Asset3.svg"
+    },
+    KeyD: {
+        url:"assets/media/svg/Asset4.svg"
+    },
+    KeyE: {
+        url:"assets/media/svg/Asset5.svg"
+    },
+    KeyF: {
+        url:"assets/media/svg/Asset6.svg"
+    },
+    KeyG: {
+        url:"assets/media/svg/Asset7.svg"
+    },
+    KeyH: {
+        url:"assets/media/svg/Asset8.svg"
+    },
+    KeyI: {
+        url:"assets/media/svg/Asset9.svg"
+    },
+    KeyJ: {
+        url:"assets/media/svg/Asset10.svg"
+    },
+    KeyK: {
+        url:"assets/media/svg/Asset11.svg"
+    },
+    KeyL: {
+        url:"assets/media/svg/Asset12.svg"
+    },
+    KeyM: {
+        url:"assets/media/svg/Asset26.svg"
+    },
+    KeyN: {
+        url:"assets/media/svg/Asset25.svg"
+    },
+    KeyO: {
+        url:"assets/media/svg/Asset15.svg"
+    },
+    KeyP: {
+        url:"assets/media/svg/Asset16.svg"
+    },
+    KeyQ: {
+        url:"assets/media/svg/Asset17.svg"
+    },
+    KeyR: {
+        url:"assets/media/svg/Asset2.svg"
+    },
+    KeyS: {
+        url:"assets/media/svg/Asset14.svg"
+    },
+    KeyT: {
+        url:"assets/media/svg/Asset13.svg"
+    },
+    KeyU: {
+        url:"assets/media/svg/Asset18.svg"
+    },
+    KeyV: {
+        url:"assets/media/svg/Asset19.svg"
+    },
+    KeyW: {
+        url:"assets/media/svg/Asset20.svg"
+    },
+    KeyX: {
+        url:"assets/media/svg/Asset21.svg"
+    },
+    KeyY: {
+        url:"assets/media/svg/Asset22.svg"
+    },
+    KeyZ: {
+        url:"assets/media/svg/Asset23.svg"
+    }
 };
+
 
 document.addEventListener('keydown', function(event){
     if (event.keyCode >= 65 && event.keyCode <= 90) {
-        var div = document.createElement('div');
-        div.classList.add('letter');
-        // add capital class
-        if (event.shiftKey == true) {
-            div.classList.add('capital');
-        };
-        div.style.backgroundColor = letters[event.code].bgColor;
-        container.appendChild(div);
+
+        var img = document.createElement('img');
+        
+        console.log(letters[event.code]['url']);
+
+        img.src = letters[event.code]['url'];
+        
+        container.appendChild(img);
+
     } else if (event.keyCode == 32) {
         var div = document.createElement('div');
         div.classList.add('space');
@@ -45,18 +101,35 @@ document.addEventListener('keydown', function(event){
     } else if (event.keyCode == 8) {
         container.removeChild(container.lastChild);
     }
-});
 
-//________________________________________________
-        // eg: a, b, z
-        // console.log("key: " + event.key);
-        // eg: KeyA, KeyB, etc
-        // console.log("code: " + event.code);
-        // eg: 65, 68, etc
-        // console.log("code: " + event.keyCode);
-        // log the color
-        // console.log("color: " + letters[event.code].bgColor);
-        // change the background color
-        // document.documentElement.style.backgroundColor = letters[event.code].bgColor;
-        // add a letter to container
-        // container.innerHTML += event.key;
+    }
+);
+
+
+
+//Simpson Button (change to random position)________________________________________
+/*simpsonButton.addEventListener("click", function(){
+    var img = document.createElement("img");
+    img.src = simpsons[Math.floor(Math.random() * 4 )];
+    simpsonWrapper.appendChild(img);
+
+    event.stopPropagation();
+})*/
+
+
+
+
+/*//BG Button (change to "Keydown"____________________________________________
+
+var bgButton = document.getElementById("bg-button");
+var html = document.documentElement;
+
+bgButton.addEventListener("keydown", function() {
+    if ( html.style.backgroundColor != "lightpink" ) {
+        html.style.backgroundColor = "lightpink";
+    } else {
+        html.style.backgroundColor = "white";
+    }
+
+    event.stopPropagation();
+*/
